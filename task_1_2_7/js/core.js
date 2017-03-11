a37023761b9c0e46c2fd031243dff6199b4af642
axisSasa <384903356@qq.com>
2017-03-11 21:11:19 +0800

    精简代码

--- task_1_2_7/js/core.js
+++ task_1_2_7/js/core.js
@@ -3,65 +3,49 @@
 		time = 0,
 		controler = document.querySelector('.control');
 
-	function isDisableClick( bool ) {
-
-		var controlers = controler.children;
-
-		for (var i = 0; i < controlers.length; i++) {
-			//h5之前的写法
-			// if (bool ) {
-			// 	controlers[i].setAttribute('disabled', 'disabled');
-			// } else {
-			// 	controlers[i].removeAttribute('disabled');
-			// }
-			//这是h5的disabled属性，为布尔值
-			controlers[i].disabled = bool;
-		}
-	}
-
-	function handler( order ) {	
-		//开始遍历前禁用点击，遍历后开启
-		//isDisableClick( true );
-		order(rootNode);
-		//isDisableClick( false );
-		//重置计时器
-		time = 0;		
-	}
-	//前序遍历
-	function preOrder( node ) {
-		if ( node ) {
-			showNode( node );
-			preOrder( node.children[0] );
-			preOrder( node.children[1] );
-		} 
-	}
-	//中序遍历
-	function inOrder( node ) {
-		if ( node ) {
-			inOrder( node.children[0] );
-			showNode( node );
-			inOrder( node.children[1] );
-		}
-	}
-	//后序遍历
-	function postOrder( node ) {
-		if ( node ) {
-			postOrder( node.children[0] );
-			postOrder( node.children[1] );
-			showNode( node );
-		}
-	}
 	//节点展示
-	function showNode( node ) {
+	function showNode( node, order ) {
 
 		setTimeout( function() {
-			node.dataset.isBeingOrder = 'order';
+			node.dataset.isBeingOrder = order;
 		}, time++ * 500);
 
 		setTimeout( function() {
 			node.dataset.isBeingOrder = '';
 		}, time * 500);
 	}
+	//前序遍历
+	var preOrder = (function preOrderFun( node ) {
+		if ( node ) {
+			showNode( node, 'preorder' );
+			preOrderFun( node.children[0] );
+			preOrderFun( node.children[1] );
+		} 
+	});
+	//中序遍历
+	var inOrder = (function inOrderFun( node ) {
+		if ( node ) {
+			inOrderFun( node.children[0] );
+			showNode( node, 'inorder' );
+			inOrderFun( node.children[1] );
+		}
+	});
+	//后序遍历
+	var postOrder = (function postOrderFun( node ) {
+		if ( node ) {
+			postOrderFun( node.children[0] );
+			postOrderFun( node.children[1] );
+			showNode( node, 'postorder' );
+		}
+	});
+
+	function handler( order ) {	
+
+		order(rootNode);
+		//重置计时器
+		time = 0;		
+	}
+
 	var handleClick = function( ev ) {
 		switch(ev.target.className) {
 			case 'preOrder' :
@@ -71,6 +55,11 @@
 				handler(inOrder);
 				break;
 			case 'postOrder' :
+				handler(postOrder);
+				break;
+			case 'together' :
+				handler(preOrder);
+				handler(inOrder);
 				handler(postOrder);
 				break;
 			default :
